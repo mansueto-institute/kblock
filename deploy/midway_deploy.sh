eb24f09ec814a47d52fb82cd50cee12eef2b769c
Nicholas Marchio <namarchio@gmail.com>
2022-01-02 21:32:34 -0600

    Update midway_deploy.sh

--- deploy/midway_deploy.sh
+++ deploy/midway_deploy.sh
@@ -14,8 +14,6 @@
 
 country_list=(AGO BDI BEN BFA BWA CAF CIV CMR COD COG COM CPV DJI ERI ESH ETH GAB GHA GIN GMB GNB GNQ KEN LBR LSO MDG MLI MOZ MRT MUS MWI NAM NER NGA RWA SDN SEN SLE SOM SSD STP SWZ SYC TCD TGO TZA UGA ZAF ZMB ZWE)
  
-cd /project2/bettencourt/mnp/analytics/scripts/kblock/dev/compute/
-
 log_file_arg=/project2/bettencourt/mnp/analytics/deployments/log_build_blocks.log
 country_chunk_arg=${country_list[@]}
 output_dir_arg=/project2/bettencourt/mnp/analytics/outputs
