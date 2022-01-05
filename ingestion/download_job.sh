99b5346c9ded0857cc2ac93065d03ddd177af42a
Nicholas Marchio <namarchio@gmail.com>
2022-01-05 13:43:35 -0600

    Update download_job.sh

--- ingestion/download_job.sh
+++ ingestion/download_job.sh
@@ -16,12 +16,6 @@ while getopts ":o:" opt; do
   esac
 done
 
-function arraydiff() {
-  awk 'BEGIN{RS=ORS=" "}
-       {NR==FNR?a[$0]++:a[$0]--}
-       END{for(k in a)if(a[k])print k}' <(echo -n "${!1}") <(echo -n "${!2}")
-}
-
 cwd=$(pwd)
 mkdir -p $outpath
 mkdir -p $outpath/osm/pbf
@@ -29,6 +23,7 @@ mkdir -p $outpath/gadm/shp
 
 # Create array of countries that haven't been downloaded from https://www.geofabrik.de/
 osm_finished=()
+shopt -s nullglob
 for i in $outpath/osm/pbf/*-latest.osm.pbf; do
     osm_finished+=("$i")
 done
@@ -39,7 +34,7 @@ echo "${osm_finished[@]}"
 geofabrik_list=(angola burundi benin burkina-faso botswana central-african-republic ivory-coast cameroon congo-democratic-republic congo-brazzaville comores cape-verde djibouti eritrea morocco ethiopia gabon ghana guinea senegal-and-gambia guinea-bissau equatorial-guinea kenya liberia lesotho madagascar mali mozambique mauritania mauritius malawi namibia niger nigeria rwanda sudan senegal-and-gambia sierra-leone somalia south-sudan sao-tome-and-principe swaziland seychelles chad togo tanzania uganda south-africa zambia zimbabwe)
 
 residual_osm_list=()
-residual_osm_list=($(arraydiff geofabrik_list[@] osm_finished[@]))
+residual_osm_list=(`echo ${geofabrik_list[@]} ${osm_finished[@]} | tr ' ' '\n' | sort | uniq -u`)
 echo "${residual_osm_list[@]}"
 
 for country in ${residual_osm_list[@]}; do
@@ -50,6 +45,7 @@ done
 
 # Create array of countries that haven't been downloaded from https://gadm.org/
 gadm_finished=()
+shopt -s nullglob
 for i in $outpath/gadm/shp/*; do
     gadm_finished+=("$i")
 done
@@ -59,13 +55,10 @@ echo "${gadm_finished[@]}"
 gadm_list=(AGO BDI BEN BFA BWA CAF CIV CMR COD COG COM CPV DJI ERI ESH ETH GAB GHA GIN GMB GNB GNQ KEN LBR LSO MDG MLI MOZ MRT MUS MWI NAM NER NGA RWA SDN SEN SLE SOM SSD STP SWZ SYC TCD TGO TZA UGA ZAF ZMB ZWE)
 
 residual_gadm_list=()
-residual_gadm_list=($(arraydiff gadm_list[@] gadm_finished[@]))
+residual_gadm_list=(`echo ${gadm_list[@]} ${gadm_finished[@]} | tr ' ' '\n' | sort | uniq -u`)
 echo "${residual_gadm_list[@]}"
 
 gadm_list_arg=${residual_gadm_list[@]}
 output_dir_arg=$outpath/gadm
 
 python $cwd/download_gadm.py --country_chunk $gadm_list_arg --output_dir $output_dir_arg
-
-
-
