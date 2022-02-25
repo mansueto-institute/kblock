58a772c858727a6c1d93985c4ccbabfe224f9e66
mmerrittsmith <matthewmerrittsmith@gmail.com>
2022-02-25 12:14:38 -0600

    deleting centroid geojsons after collecting into parquet

--- deploy/4-calculate-pop/centroidize_buildings/centroidize.py
+++ deploy/4-calculate-pop/centroidize_buildings/centroidize.py
@@ -21,16 +21,17 @@ def centroidize(top_building_dir: Union[str, Path]):
 	building_files_list = top_building_dir.rglob("buildings*")
 	map(centroidize_helper, list(building_files_list))
 	country_building_dirs = [d for d in top_building_dir.iterdir() if d.is_dir()]
-	for country_building_dir in country_building_dirs:
-		aggregate_helper(country_building_dir)
 	map(aggregate_helper, country_building_dirs)
+	centroid_files_list = top_building_dir.rglob("centroid_*")
+	for centroid_geojson in centroid_files_list:
+		if 'parquet' not in centroid_geojson.name:
+			centroid_geojson.unlink()
 
 
 	
 def centroidize_helper(building_file_path: Path):
 	building_file = gpd.read_file(building_file_path).set_crs(3395, allow_override=True)
-	building_file = dgpd.from_geopandas(building_file, chunksize=100)
-	building_file['building_area'] = building_file.area.compute()
+	building_file['building_area'] = building_file.area
 	building_file.geometry = building_file.geometry.centroid
 	building_file = building_file.set_crs(4326, allow_override=True)
 	building_file = building_file[['osm_id', 'gadm', 'gadm_code', 'building_area', 'geometry']]
@@ -39,7 +40,9 @@ def centroidize_helper(building_file_path: Path):
 
 
 def aggregate_helper(country_building_dir: Path):
-	# print(country_building_dir.iterd)
-	all_building_files = pd.concat([gpd.read_file(building_file_path.resolve()) for building_file_path in country_building_dir.iterdir() if 'centroid' in building_file_path.name])
+	all_building_files = pd.concat([gpd.read_file(building_file_path.resolve()) for building_file_path in country_building_dir.iterdir() if 'centroid' in building_file_path.name and 'geojson' in building_file_path.name])
 	all_building_files = dgpd.from_geopandas(all_building_files, chunksize=100)
-	all_building_files.to_parquet(country_building_dir / (country_building_dir.name+'_centroids.parquet'))
+	output_dir = country_building_dir.parent.parent / 'building_centroids' / (country_building_dir.name + '_centroids')
+	if not output_dir.exists():
+		output_dir.mkdir(parents=True)
+	all_building_files.to_parquet(output_dir)
