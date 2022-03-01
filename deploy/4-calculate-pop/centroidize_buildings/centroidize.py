6d67817eda0f7f00d4cf23ceea2e57f9743b3018
mmerrittsmith <matthewmerrittsmith@gmail.com>
2022-03-01 17:23:05 -0600

    updating aggregation step

--- deploy/4-calculate-pop/centroidize_buildings/centroidize.py
+++ deploy/4-calculate-pop/centroidize_buildings/centroidize.py
@@ -28,7 +28,10 @@ def centroidize(top_building_dir: Union[str, Path]):
         # centroidize_helper(building_file)
     list(map(centroidize_helper, building_files_list))
     top_centroid_dir = top_building_dir.parent / 'building_centroids'
-    # country_building_dirs = [d for d in top_centroid_dir.iterdir() if d.is_dir()]
+    country_building_dirs = [d for d in top_centroid_dir.iterdir() if d.is_dir()]
+    top_level_bounds = list(set([file.name.split('_')[1].split('.')[1] for file in top_centroid_dir.rglob("building_centroids*")]))
+    top_level_bounds = zip(top_level_bounds, top_centroid_dir*len(top_level_bounds))
+    # pass a tuple of the top level building_centroid directory and a string that's of the form countrycode
     # list(map(aggregate_helper, country_building_dirs))
     # centroid_files_list = top_building_dir.rglob("centroid_*")
     # for centroid_geojson in centroid_files_list:
@@ -58,15 +61,18 @@ def centroidize_helper(building_file_path: Path):
     building_file.to_file(output_file_name, driver='GeoJSON')
 
 
-def aggregate_helper(country_building_dir: Path):
+def aggregate_helper(aggregation_tuple):
     logging.getLogger().setLevel(logging.INFO)
     log_file = Path.cwd() / 'log.txt'
     logging.basicConfig(filename=log_file, format='%(asctime)s:%(message)s: ', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
-    logging.info(f'Working on {country_building_dir}')
-    all_building_files = pd.concat([gpd.read_file(building_file_path.resolve()) for building_file_path in country_building_dir.iterdir() if 'centroid' in building_file_path.name and 'geojson' in building_file_path.name])
-    all_building_files = dgpd.from_geopandas(all_building_files, npartitions=10)
-    output_dir = country_building_dir.parent.parent / 'building_centroid_parquets' / (country_building_dir.name + '_centroids')
-    if not output_dir.exists():
-        output_dir.mkdir(parents=True)
+    top_level_bounds = aggregation_tuple[0]
+    building_centroid_dir = aggregation_tuple[1]
+    logging.info(f'Working on {top_level_bounds}')
+    country_centroid_dir = Path(building_centroid_dir) / top_level_bounds.split('.')[0]
+    if not country_centroid_parquet_dir.exists():
+        country_centroid_parquet_dir.mkdir(parents=True)
+
+    all_building_files = pd.concat([gpd.read_file(centroid_path.resolve()) for centroid_path in country_centroid_dir.rlob("centroid_buildings_"+top_level_bounds+"*")])
+    output_path = Path(building_centroid_dir).parent / 'building_centroid_parquets' / top_level_bounds.split('.')[0] / (top_level_bounds.split('.')[1]+'.parquet')
     all_building_files.to_parquet(output_dir)
     logging.info(f'Done working on {country_building_dir}')
